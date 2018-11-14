521c6fc2bf3d05319f969b43e0c0f91159bb7c13
halbu <fstream.c@gmail.com>
2018-11-14 23:17:37 +0000

    Sprite data

--- race/race.lua
+++ race/race.lua
@@ -8,13 +8,8 @@ sw, sh = 240, 136
 trackWidth = 0.8
 trueCentre = 0.5
 
-chevronCounter = 0
-chevronLength = 15
-chevronWidth = 0.075 -- width of barrier thing on either side
-
-stripeCounter = 0
-stripeLength = 40
-stripeWidth = 0.02
+chevronCounter, chevronLength, chevronWidth = 0, 15, 0.075
+stripeCounter, stripeLength, stripeWidth = 0, 40, 0.02
 
 speed = 0.0
 topSpeed = 3.1
@@ -23,7 +18,7 @@ curveDir = 3
 curveChangeSpeed = 0.005
 maxCurve = 0.6
 grassColor, roadColor = 3, 2
-car_x = 0.5
+carX = 0.5
 xv = 0
 dir = 0
 turnSpeed = 0.0005
@@ -31,8 +26,7 @@ maxTurnSpeed = 0.0125
 horizon = sh/2
 
 -- aliases as these improve performance somehow?
-sin = math.sin
-flr = math.floor
+sin, flr = math.sin, math.floor
 
 function TIC()
  if speed < topSpeed then speed = speed + 0.01 end
@@ -45,17 +39,21 @@ function TIC()
  drawTrack()
  handleInput()
  
- spr(dir, car_x * sw - 16, 100, 7, 2, 0, 0, 2, 2)
+ spr(dir, carX * sw - 16, 100, 7, 2, 0, 0, 2, 2)
 
- chevronCounter=chevronCounter+speed
- if chevronCounter>chevronLength then chevronCounter=chevronCounter-chevronLength end
+ chevronCounter = chevronCounter + speed
+ if chevronCounter > chevronLength then
+  chevronCounter = chevronCounter - chevronLength
+ end
 
- stripeCounter=stripeCounter+speed
- if stripeCounter>stripeLength then stripeCounter=stripeCounter-stripeLength end
+ stripeCounter = stripeCounter + speed
+ if stripeCounter > stripeLength then
+  stripeCounter = stripeCounter - stripeLength
+ end
 end
 
 function carDrift()
- car_x = car_x - ((curve/50) * speed) / topSpeed
+ carX = carX - ((curve/50) * speed) / topSpeed
 end
 
 function updateCurve()
@@ -85,7 +83,7 @@ function handleInput()
  end
  if xv > maxTurnSpeed then xv = maxTurnSpeed end
  if xv < -maxTurnSpeed then xv = -maxTurnSpeed end
- car_x = car_x + xv
+ carX = carX + xv
  if xv > 0.004 then dir = 4 end
  if xv < -0.004 then dir = 2 end
 end
@@ -126,3 +124,32 @@ function drawTrack()
   print(flr(speed * 36)..'mph', 2, 2, 15, true, 1, false)
  end
 end
+-- <TILES>
+-- 000:7777777777777777777777777777777777777777777777777776666677610000
+-- 001:7777777777777777777777777777777777777777777777776666677700000677
+-- 002:7777777777777777777777777777777777777777777777777776666677061100
+-- 003:7777777777777777777777777777777777777777777777776666667700000667
+-- 004:7777777777777777777777777777777777777777777777777766666676610000
+-- 005:7777777777777777777777777777777777777777777777776666677700006077
+-- 006:7777755577755555775555557755555577755550775555557555555575055555
+-- 007:5577777755557777555557775555557755555557555555575555555755555557
+-- 016:761001007610000066666666600ccccc66666666600000006666666670000077
+-- 017:000000670000006766666666ccccc00666666666000000066666666677000007
+-- 018:700610010006100066666666666600cc66666666660000000666666600000077
+-- 019:000000660000006666666666ccccc00666666666000000666666666777000007
+-- 020:661001006610000066666666600ccccc66666666660000007666666670000077
+-- 021:000060070000600066666666cc00666666666666000000666666666077000000
+-- 022:7555555577555555777555557777770077777770777777707777777077777777
+-- 023:5555557755555777555577770777777707777777077777770777777777777777
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <PALETTE>
+-- 000:140c1c44243430346d4e4a4e854c30346524d04648757161597dced27d2c8595a16daa2cd2aa996dc2cadad45edeeed6
+-- </PALETTE>
+
