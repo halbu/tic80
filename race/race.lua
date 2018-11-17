df7d87910c495c3c6b6c6ab8f3aa9298082e7a56
halbu <fstream.c@gmail.com>
2018-11-17 22:06:12 +0000

    Trees. Minor gui changes

--- race/race.lua
+++ race/race.lua
@@ -4,6 +4,7 @@
 -- script: lua
 
 sw, sh = 240, 136
+cx, cy, cr = sw - 20, 20, 18 -- speedometer circle x / y / radius
 
 trackWidth = 0.8
 trueCentre = 0.5
@@ -27,6 +28,7 @@ horizon = sh / 2
 horizonXOffset = 0
 
 bollards = {}
+trees = {}
 dist = 0
 timer = 3659
 
@@ -57,12 +59,19 @@ function TIC()
   stripeCounter = stripeCounter - stripeLength
  end
 
- preDist = dist
+ pDist = dist
  dist = dist + (speed / 2)
- if (preDist<60 and dist>60) or (preDist<80 and dist>80) or (preDist<100 and dist>100) then
-  bollard = {side=0, y=horizon}
-  table.insert(bollards, bollard)
+ if (pDist < 60 and dist > 60) or (pDist < 80 and dist > 80) or (pDist < 100 and dist > 100) then
+  table.insert(bollards, {y=horizon})
  end
+ 
+ if math.random() >= 0.98 then
+  if math.random() < (speed/topSpeed) then
+   treeSide = math.random(0, 1)
+   table.insert(trees, {side=treeSide, y=horizon})
+  end
+ end
+
  if dist > 100 then dist = dist - 100 end
 
  timer = timer - 1
@@ -92,6 +101,9 @@ function updateTrack()
 
  horizonXOffset = horizonXOffset - ((curve / 6) * (speed / topSpeed)) -- scroll horizon
  for k,v in pairs(bollards) do
+  v.y = v.y + (speed / 2)
+ end
+ for k,v in pairs(trees) do
   v.y = v.y + (speed / 2)
  end
 end
@@ -155,7 +167,6 @@ function drawTrack()
   brkPt4 = (horizon + ((sh-horizon)/10)*7)
 
   for k, v in pairs(bollards) do
-   ground = sh - horizon
    if flr(v.y) == j then
     bollardLeft = chevronLeft - (0.1 * perspSqz)
     bollardRight = chevronRight + (0.1 * perspSqz)
@@ -169,6 +180,24 @@ function drawTrack()
      drawBollardPair(bollardLeft, bollardRight, v.y, 1.5)
     elseif v.y > horizon then
      drawBollardPair(bollardLeft, bollardRight, v.y, 1)
+    end
+   end
+  end
+
+  for k, v in pairs(trees) do
+   if flr(v.y) == j then
+    treeX = chevronLeft - (0.075 * perspSqz)
+    if v.side == 1 then treeX = chevronRight + (0.075 * perspSqz) end
+    if v.y > brkPt4 then
+     spr(14, treeX * sw - 8, v.y - 32, 7, 2, 0, 0, 2, 2)
+    elseif v.y > brkPt3 then
+     spr(14, treeX * sw - 8, v.y - 32, 7, 2, 0, 0, 2, 2)
+    elseif v.y > brkPt2 then
+     spr(12, treeX * sw - 8, v.y - 32, 7, 2, 0, 0, 2, 2)
+    elseif v.y > brkPt1 then
+     spr(7, treeX * sw - 4, v.y - 16, 7, 2, 0, 0, 1, 1)
+    elseif v.y > horizon then
+     spr(6, treeX * sw - 4, v.y - 16, 7, 2, 0, 0, 1, 1)
     end
    end
   end
@@ -186,27 +215,30 @@ function drawBollard(x, y, s)
  rect(x * sw, y - s * 4, s, s, 8)
 end
 
-function drawGui()
- print(flr(speed * 36)..'mph', 2, 2, 15, true, 1, false)
- tw = print(flr(timer/60), 100, -60, 14, true, 2, false)
- print(flr(timer/60), sw / 2 - (tw / 2) - 1, 2, 14, true, 2, false)
- print(flr(timer/60), sw / 2 - (tw / 2), 3, 15, true, 2, false)
+function drawTextCentredAtPoint(str, x, y, color, scale, smallFont)
+ tw = print(str, x, -10, color, false, scale, smallFont)
+ print(str, x - tw / 2, y, color, false, scale, smallFont)
+end
 
- cx, cy, cr = sw - 20, 20, 18
+function drawGui()
+ drawTextCentredAtPoint(flr(timer/60), sw / 2 - 1, 2, 14, 2, false)
+ drawTextCentredAtPoint(flr(timer/60), sw / 2, 2, 15, 2, false)
+
  circb(cx, cy, cr, 15)
+ drawTextCentredAtPoint(flr(speed * 36)..'mph', cx, cy+cr+5, 15, 1, false)
 
  speedometerStartAngle = 135
 
  -- speedometer outline. i'd cache these values if i wasn't lazy
  for i = 0, 300, 30 do
-  x1 = math.cos((speedometerStartAngle - 15 + i)/360 * twoPi) * 11 + 0.5
-  x2 = math.cos((speedometerStartAngle - 15 + i)/360 * twoPi) * (cr - 3) + 0.5
-  y1 = math.sin((speedometerStartAngle - 15 + i)/360 * twoPi) * 11 + 0.5
-  y2 = math.sin((speedometerStartAngle - 15 + i)/360 * twoPi) * (cr - 3) + 0.5
+  x1 = math.cos((speedometerStartAngle - 15 + i) / 360 * twoPi) * 11 + 0.5
+  x2 = math.cos((speedometerStartAngle - 15 + i) / 360 * twoPi) * (cr - 3) + 0.5
+  y1 = math.sin((speedometerStartAngle - 15 + i) / 360 * twoPi) * 11 + 0.5
+  y2 = math.sin((speedometerStartAngle - 15 + i) / 360 * twoPi) * (cr - 3) + 0.5
   line(cx + x1, cy + y1, cx + x2, cy + y2, 7)
  end
 
- -- speedometer
+ -- speedometer needle
  speedoLength = 13
  speedoAngle = speedometerStartAngle + (speed * 90)
  dx = math.cos(speedoAngle/360 * twoPi) * speedoLength
@@ -216,7 +248,6 @@ function drawGui()
  line(cx, cy, cx + dx, cy + dy, 15)
  pix(cx + dx, cy + dy, 8)
 end
-
 -- <TILES>
 -- 000:7777777777777777777777777777777777777777777777777776666677610000
 -- 001:7777777777777777777777777777777777777777777777776666677700000677
@@ -224,24 +255,32 @@ end
 -- 003:7777777777777777777777777777777777777777777777776666667700000667
 -- 004:7777777777777777777777777777777777777777777777777766666676610000
 -- 005:7777777777777777777777777777777777777777777777776666677700006077
--- 006:7777755577755555775555557755555577755550775555557555555575055555
--- 007:5577777755557777555557775555557755555557555555575555555755555557
+-- 006:7777777777777777777777777775577777755777775555777755557777744777
+-- 007:7775577777755777775555777755557777555577755555577555555777744777
 -- 008:7777777777777777777777777777777777777777777777777777777777777777
 -- 009:7777777777777777777777777777777777777777777777777777777777777777
 -- 010:7777777777777777777777777777777777777777777777777777777777777777
 -- 011:7777777777777777777777777777777777777777777777777777777777777777
+-- 012:7777777777777777777777777777777777777775777777557777777577777755
+-- 013:7777777777777777777777777777777757777777557777775777777755777777
+-- 014:7777777577777775777777557777755577775555777555557777755577775555
+-- 015:5777777757777777557777775557777755557777555557775557777755557777
 -- 016:761001007610000066666666600eeeee66666666600000006666666670000077
 -- 017:000000670000006766666666eeeee00666666666000000066666666677000007
 -- 018:700610010006100066666666666600ee66666666660000000666666600000077
 -- 019:000000660000006666666666eeeee00666666666000000666666666777000007
 -- 020:661001006610000066666666600eeeee66666666660000007666666670000077
 -- 021:000060070000600066666666ee00666666666666000000666666666077000000
--- 022:7555555577555555777555557777770077777770777777707777777077777777
--- 023:5555557755555777555577770777777707777777077777770777777777777777
+-- 022:7555555577555555777555557777774477777774777777747777777477777777
+-- 023:5555557755555777555577774777777747777777477777774777777777777777
 -- 024:7777777777777777777777777777777777777777777777777777777777777777
 -- 025:7777777777777777777777777777777777777777777777777777777777777777
 -- 026:7777777777777777777777777777777777777777777777777777777777777777
 -- 027:7777777777777777777777777777777777777777777777777777777777777777
+-- 028:7777755577775555777775557777555577755555775555557777774477777744
+-- 029:5557777755557777555777775555777755555777555555774477777744777777
+-- 030:7775555575555555775555555555555555555555777774447777744477777444
+-- 031:5555577755555557555555775555555755555555444777774447777744477777
 -- 040:7777777777777777777777777777777777777777777777777777777777777777
 -- 041:7777777777777777777777777777777777777777777777777777777777777777
 -- 042:7777777777777777777777777777777777777777777777777777777777777777
